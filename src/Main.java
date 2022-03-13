a582d7c32bdfb143c2afe24fd2a7218c04268afe
Anton Morozov <y.anton.morozov@yandex.ru>
2022-03-13 12:27:08 +0300

    удалить import неиспользуемых библиотек

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ import manager.TaskManager;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,7 +3,6 @@ package manager;
 import task.Task;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager{
     private final ArrayList<Task> history;
